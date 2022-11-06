99bcfa5ec56e35462fa6cc97f3ebc358dbe48242
beaux <34432201+beaux-riel@users.noreply.github.com>
2022-11-05 22:12:12 -0700

    Day 1 - Minor breaking changes noticed

--- day1.sol
+++ day1.sol
@@ -15,3 +15,24 @@ contract Inbox {
         return message;
     }
 }
+
+
+
+// Some changes required due to new language rules (eg. using "constructor")
+// pragma solidity ^0.4.17;
+
+// contract Inbox {
+//     string public message;
+    
+//     constructor(string initialMessage) public {
+//         message = initialMessage;
+//     }
+    
+//     function setMessage(string newMessage) public {
+//         message = newMessage;
+//     }
+    
+//     function getMessage() public view returns (string) {
+//         return message;
+//     }
+// }
