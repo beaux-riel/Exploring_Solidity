fe33f3830052139ec75a2f45f71183ed4d5a52df
beaux <34432201+beaux-riel@users.noreply.github.com>
2022-11-06 19:59:27 -0800

    Day 2 - Simple HelloWorld and a few basics

---
+++ HelloWorld.sol
@@ -0,0 +1,9 @@
+// Note: All exercises were tested at remix.ethereum.org and exported to VS Code
+
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.13;
+contract HelloWorld {
+    function sayHelloWorld() public pure returns (string memory) {
+        return "Hello World";
+    }
+}
--- day1.sol
+++ day1.sol
@@ -1,4 +1,6 @@
-pragma solidity ^0.4.17;
+// Note: All exercises were tested at remix.ethereum.org and exported to VS Code
+
+pragma solidity 0.4.17;
 
 contract Inbox {
     string public message;
@@ -10,16 +12,18 @@ contract Inbox {
     function setMessage(string newMessage) public {
         message = newMessage;
     }
-
-    function getMessage() public view returns (string) {
-        return message;
-    }
 }
 
+    // The below function is superfluous as within Solidity, the line: 
+    // "string public message;" creates a function linked to that 
+    // variable automagically.
+    // function getMessage() public view returns (string) {
+    //     return message;
+    // }
 
 
 // Some changes required due to new language rules (eg. using "constructor")
-// pragma solidity ^0.4.17;
+// pragma solidity 0.4.17;
 
 // contract Inbox {
 //     string public message;
