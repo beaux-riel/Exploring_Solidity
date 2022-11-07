1f43fc7d5ac047d5904f8595c66255f1fa3dc11b
beaux <34432201+beaux-riel@users.noreply.github.com>
2022-11-06 20:36:24 -0800

    General cleaning

--- day1.sol
+++ day1.sol
@@ -1,14 +1,10 @@
 // Note: All exercises were tested at remix.ethereum.org and exported to VS Code 
-
 pragma solidity 0.4.17;
-
 contract Inbox {
     string public message;
-
     function Inbox(string initialMessage) public {
         message = initialMessage;
     }
-
     function setMessage(string newMessage) public {
         message = newMessage;
     }
--- day2.sol
+++ day2.sol
@@ -1,18 +1,13 @@
 // Note: All exercises were tested at remix.ethereum.org and exported to VS Code
-
 pragma solidity 0.8.4;
-
 contract Inbox {
     string public message;
-
-    constructor(string initialMessage) public {
+        constructor(string initialMessage) public {
         message = initialMessage;
     }
-
     function setMessage(string newMessage) public {
         message = newMessage;
     }
-
     // This section was in relation to a discussion around gas
     function doMath(int a, int b) {
         a + b;
